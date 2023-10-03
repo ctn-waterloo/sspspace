40a9f4aa049adbcd4987491e6d8b83f03ad85ced
Michael Furlong <furlong@gmail.com>
2023-10-03 14:37:05 -0400

    Fixed missing comma in import

--- sspspace/__init__.py
+++ sspspace/__init__.py
@@ -1,4 +1,4 @@
 from .ssp import SSP, bind, invert
 from .encoders import SSPEncoder, RandomSSPSpace, HexagonalSSPSpace
 from .decoders import SSPDecoder, SSPSimilarityDecoder, train_decoder_net_tf, train_decoder_net_sk
-from .util import sample_domain, conjugate_symmetry, make_good_unitary vecs_from_phases
+from .util import sample_domain, conjugate_symmetry, make_good_unitary, vecs_from_phases

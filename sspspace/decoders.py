7ed4038bc99e714cdb9740fbf2657923e71a903d
kpc-simone <kpc.simone@gmail.com>
2023-10-06 15:55:29 -0400

    add optional noise to SSP training examples for sk decoder network

--- sspspace/decoders.py
+++ sspspace/decoders.py
@@ -37,32 +37,36 @@ class SSPSimilarityDecoder:
         self.sim_ssps = sim_ssps
         self.encoder = encoder
 
-    def decode(self, ssps):
+    def decode(self, ssps, optimize = True):
         sims = self.sim_ssps | ssps
         
         x0 = self.sim_xs[np.argmax(sims, axis=0),:]
-        solns = np.zeros(x0.shape)
-        for i in range(x0.shape[0]):
-            def min_func(x,target=ssps[i,:]):
-                x_ssp = self.encoder.encode(np.atleast_2d(x))
-                return -np.inner(x_ssp, target).flatten()
-            soln = minimize(min_func, x0[i,:], 
-                        method='L-BFGS-B',
-#                         bounds=self.domain_bounds,
-            )
-            solns[i,:] = soln.x
-    
-        return solns
+        if optimize == True:
+            solns = np.zeros(x0.shape)
+            for i in range(x0.shape[0]):
+                def min_func(x,target=ssps[i,:]):
+                    x_ssp = self.encoder.encode(np.atleast_2d(x))
+                    return -np.inner(x_ssp, target).flatten()
+                soln = minimize(min_func, x0[i,:], 
+                            method='L-BFGS-B',
+    #                         bounds=self.domain_bounds,
+                )
+                solns[i,:] = soln.x
+            return solns
+        else:
+            return x0
 
 
-def train_decoder_net_sk(encoder, bounds, n_training_pts=200000,
-                      sample_points=None,
-                      hidden_units = [8],
-                      learning_rate=1e-3, 
-                      n_epochs = 100, 
-                      patience=3,
-                      tolerance=1e-6,
-                      verbose=True):
+def train_decoder_net_sk(encoder, bounds, n_training_pts = 200000, 
+                        corrupt_training_examples = True,
+                        gaussian_noise_std = 'auto',
+                        sample_points = None,
+                        hidden_units = [8],
+                        learning_rate = 1e-3, 
+                        n_epochs = 100, 
+                        patience = 3,
+                        tolerance = 1e-6,
+                        verbose = True):
     '''
         Trains a dense neural network to decode SSPs.
 
@@ -84,13 +88,20 @@ def train_decoder_net_sk(encoder, bounds, n_training_pts=200000,
     )
 
 
-   
+    print('Generating training examples ...')
     if sample_points is None:
         sample_points = sample_domain(bounds, n_training_pts)
+        print(sample_points.shape)
     sample_ssps = encoder.encode(sample_points)
+    
+    if corrupt_training_examples == True:
+        if gaussian_noise_std == 'auto':
+            gaussian_noise_std = 1/np.sqrt(encoder.ssp_dim)
+        sample_ssps += np.random.normal(loc = 0., scale = gaussian_noise_std, size = sample_ssps.shape) 
 
 #     shuffled_ssps, shuffled_pts = sklearn.utils.shuffle(sample_ssps, sample_points)
 
+    print('Training decoder network ...')
     model.fit(sample_ssps, sample_points)
 
     history = {

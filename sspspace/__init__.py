8771cfb9b404c9d45b97e1f5c93f17817059c48c
P. Michael Furlong <michael.furlong@uwaterloo.ca>
2024-03-08 17:06:50 -0500

    Added similarity decoder and the new training functions from the no-bounds-opt branch

--- sspspace/__init__.py
+++ sspspace/__init__.py
@@ -1,4 +1,4 @@
 from .ssp import SSP
 from .encoders import SSPEncoder, RandomSSPSpace, HexagonalSSPSpace, DiscreteSPSpace
-from .decoders import SSPDecoder, train_decoder_net
+from .decoders import SSPDecoder, SSPSimilarityDecoder, train_decoder_net_sk, train_decoder_net_tf
 from .util import vecs_from_phases

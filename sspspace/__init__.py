bed629d97b2b26428781369de2a0d3edea822b9e
Michael Furlong <furlong@gmail.com>
2024-03-08 09:20:24 -0500

    exposed function call versions of operators

--- sspspace/__init__.py
+++ sspspace/__init__.py
@@ -1,4 +1,4 @@
-from .ssp import SSP
+from .ssp import SSP, sim, bind, invert
 from .encoders import SSPEncoder, RandomSSPSpace, HexagonalSSPSpace, DiscreteSPSpace
 from .decoders import SSPDecoder, train_decoder_net
 from .util import vecs_from_phases

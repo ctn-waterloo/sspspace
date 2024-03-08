8bcd1d1125ba22f2bf30edc750b4aebeb4e86cff
P. Michael Furlong <michael.furlong@uwaterloo.ca>
2024-03-08 16:45:03 -0500

    plucked the proper encoding scheme for DiscreteSPSpace encoder from no-bounds-opt branch

--- sspspace/encoders.py
+++ sspspace/encoders.py
@@ -11,50 +11,45 @@ def k_to_vector(K):
 
 
 class DiscreteSPSpace:
-    def __init__(self, keys, ssp_dim, optimize=False, mul=1):
+    def __init__(self, keys, ssp_dim):
+        self.ssp_dim = ssp_dim 
+        self.length_scale = np.array([1])
         self.keys = keys
-        if not optimize:
-            self.ssp_dim = ssp_dim
-            self.map = SSP([make_good_unitary(ssp_dim, mul=1) for k in self.keys])
-        else:
-            self.ssp_dim = ssp_dim + 2
-            self.map = SSP(np.zeros((len(self.keys), self.ssp_dim)))
+#         self.map = SSP([make_good_unitary(ssp_dim) for k in self.keys])
 
-            phase0 = np.random.uniform(low=-np.pi, high=np.pi, 
-                                       size=(1, (self.ssp_dim-2)//2))
+        self.map = SSP(np.zeros((len(self.keys), self.ssp_dim)))
 
-            self.map[0,:] = k_to_vector(phase0)
-            
-            def greedy_min_func(x, vecs):
-                K = x.reshape((1,vecs.shape[1]//2 - 1))
-                phi = k_to_vector(K)    
-                sims = np.einsum('nd,md->nm', phi, vecs)
-                return np.linalg.norm(sims)
+        phase0 = np.random.uniform(low=-np.pi, high=np.pi, 
+                                   size=(1, (self.ssp_dim-2)//2))
 
-            from scipy.optimize import minimize
-           
-            for i in range(1,len(self.keys)):
-                x0 = np.random.uniform(low=-np.pi, 
-                                       high=np.pi, 
-                                       size=((self.ssp_dim -2)// 2,))
-                greedy_soln = minimize(greedy_min_func, x0, 
-                                       args=(self.map[:i,:]), 
-                                       method='L-BFGS-B')
-                self.map[i,:] = k_to_vector(greedy_soln.x.reshape((1,(self.ssp_dim-2)//2)))
-            ### end for
-        ### end if
+        self.map[0,:] = k_to_vector(phase0)
+        
+        def greedy_min_func(x, vecs):
+            K = x.reshape((1,vecs.shape[1]//2 - 1))
+            phi = k_to_vector(K)    
+            sims = np.einsum('nd,md->nm', phi, vecs)
+            return np.linalg.norm(sims)
+
+        from scipy.optimize import minimize
+       
+        for i in range(1,len(self.keys)):
+            x0 = np.random.uniform(low=-np.pi, 
+                                   high=np.pi, 
+                                   size=((self.ssp_dim -2)// 2,))
+            greedy_soln = minimize(greedy_min_func, x0, 
+                                   args=(self.map[:i,:]), 
+                                   method='L-BFGS-B')
+            self.map[i,:] = k_to_vector(greedy_soln.x.reshape((1,(self.ssp_dim-2)//2)))
     ### end __init__
 
-    def from_codebook(self, codebook):
-        self.ssp_dim = codebook.shape[1]
-        self.map = codebook
-        self.keys = list(range(codebook.shape[0]))
 
-
-    def encode(self, v):
-        if v not in self.keys:
-            raise RuntimeWarning(f'Key {v} is not in the dictionary')
-        return self.map[self.keys.index(v),:].reshape((1,-1))
+    def encode(self, vals):
+        retval = np.zeros((vals.shape[0], self.ssp_dim))
+        for v_idx, v in enumerate(vals): 
+            if v not in self.keys:
+                raise RuntimeWarning(f'Key {v} is not in the dictionary')
+            retval[v_idx,:] = self.map[self.keys.index(v),:].reshape((1,-1))
+        return SSP(retval)
 
     def decode(self, ssp):
         return self.keys[np.argmax(self.map | ssp)]

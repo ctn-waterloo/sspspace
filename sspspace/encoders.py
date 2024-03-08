Fixed gradient computation for ssps

sspspace/encoders.py
@@ -138,13 +138,14 @@ class SSPEncoder:
 
         '''
 
-        phi_fourier = np.fft.fft(phi, axis=0)
+        phi_fourier = np.fft.fft(phi, axis=1)
         ls_mat = np.atleast_2d(np.diag(1 / self.length_scale.flatten()))
         # d/dx[e^iAx] = hadamard(iA, e^{iAx})
         deriv_mat = 1.j * (self.phase_matrix @ ls_mat) # Derivative coeff
+        print(deriv_mat.shape)
 
-        fourier_grad = np.einsum('dm,nd->nmd',deriv_mat,phi_fourier)
-        return np.fft.ifft(fourier_grad, axis=2)
+        fourier_grad = np.einsum('dm,d->md',deriv_mat,phi_fourier.flatten())
+        return np.fft.ifft(fourier_grad, axis=1)
 
     
     def encode_and_deriv(self,x):

tests/test_encoders.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../')
 
-from sspspace import SSP, SSPEncoder, RandomSSPSpace, HexagonalSSPSpace
+from sspspace import SSP, SSPEncoder, RandomSSPSpace, HexagonalSSPSpace, train_decoder_net
 import numpy as np
 
 
@@ -37,13 +37,35 @@ def test_hexagonal_encoding():
     # TODO: Figure out numerical comparison
 
 def test_gradient():
+#     def sinc_deriv(x):
+#         return np.divide(np.cos(x) - np.sin(x), x)
+# 
+#     def exact_grad(xs):
+#         return np.array([sinc_deriv(np.pi*xs[:,0]) * np.sinc(np.pi*xs[:,1]),  np.sinc(np.pi*xs[:,0])*sinc_deriv(np.pi*xs[:,1])])
 
-    x = np.array([[1,1]])
-    rand_encoder = RandomSSPSpace(domain_dim=2,ssp_dim=128)
+    x = np.array([[1.2,0.9]])
+#     rand_encoder = RandomSSPSpace(domain_dim=2,ssp_dim=128*256)
+    rand_encoder = HexagonalSSPSpace(domain_dim=2,n_rotates=5, n_scales=5)
+
+#     decoder,_ = train_decoder_net(rand_encoder, np.array([[-4,4],[-4,4]]), n_epochs=20)
 
     phi = rand_encoder.encode(x)
     grad = rand_encoder.gradient(phi)
+
     print(grad)
+#     print(decoder.decode(phi))
+    origin = rand_encoder.encode([[0,0]])
+
+    def f(x, enc=rand_encoder, org=origin):
+        return np.dot(enc.encode(x).flatten(), org.flatten())
+
+    from scipy.optimize import approx_fprime
+    print('approx fprime', approx_fprime(x.flatten(),f))
+    print('calc fprime', grad @ origin.T)
+#     print('exact fprime',exact_grad(x))
+
+if __name__=='__main__':
+    test_gradient()
 
 #     import matplotlib.pyplot as plt
 #     plt.imshow(square_sims)